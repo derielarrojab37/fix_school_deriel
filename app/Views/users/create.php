ab856ce135a2f6dd12103b997a4fb5e1b24b899f
derielarrojab37 <deriel.arrojab37@smk.belajar.id>
2026-04-09 08:52:10 +0700

    up

--- app/Views/users/create.php
+++ app/Views/users/create.php
@@ -46,9 +46,9 @@
                         <label class="form-label">Role</label>
                         <select name="role" class="form-control" required>
                             <option value="">-- Pilih Role --</option>
-                            <option value="admin">Admin</option>
                             <option value="user">User</option>
-                            <option value="user">Teknisi</option>
+                            <option value="admin">Admin</option>
+                            <option value="teknisi">Teknisi</option>
                         </select>
                     </div>
 
--- app/Views/users/edit.php
+++ app/Views/users/edit.php
@@ -29,8 +29,8 @@
                 <div class="mb-3">
                     <label class="form-label">Role</label>
                     <select name="role" class="form-control">
-                        <option value="admin" <?= $user['role'] == 'admin' ? 'selected' : '' ?>>Admin</option>
                         <option value="user" <?= $user['role'] == 'user' ? 'selected' : '' ?>>User</option>
+                        <option value="admin" <?= $user['role'] == 'admin' ? 'selected' : '' ?>>Admin</option>
                         <option value="teknisi" <?= $user['role'] == 'teknisi' ? 'selected' : '' ?>>Teknisi</option>
                     </select>
                 </div>
